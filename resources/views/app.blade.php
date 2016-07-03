4a6f1415125e5198864cef1bfe3867e367aef9bc
Vongola <vongola12324@coder.tw>
2016-07-03 13:54:41 +0800

    Finish Index(Might need some modify on navbar or menu)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         {{-- Metatag --}}
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
         <title>@yield('title')::HackerSir CTF</title>
 
@@ -14,17 +14,33 @@
     </head>
     <body class="pushable">
         {{-- Navbar --}}
-        <div class="ui large top fixed menu transition hidden">
-            <div class="ui container">
-                <a class="active item">首頁</a>
-                <a class="item">題目</a>
-                <a class="item">關於</a>
-                <div class="right menu">
-                    <div class="item">
-                        <a class="ui button">Log in</a>
-                    </div>
+        <div class="ui large pointing inverted top menu transition hidden" style="z-index: 2;">
+            <a class="item" href="javascript:void(0)">首頁</a>
+            <a class="item" href="javascript:void(0)">題目</a>
+            <a class="item" href="javascript:void(0)">關於</a>
+            <div class="right menu">
+                <div class="item">
+                    <a class="ui button" href="javascript:void(0)">登入/註冊</a>
                 </div>
             </div>
+        </div>
+        <div class="ui sidebar inverted vertical labeled icon menu" style="z-index: 2;">
+            <a class="item" href="javascript:void(0)" style="color: white;">
+                <i class="home icon"></i>
+                首頁
+            </a>
+            <a class="item" href="javascript:void(0)">
+                <i class="book icon"></i>
+                題目
+            </a>
+            <a class="item" href="javascript:void(0)">
+                <i class="info icon"></i>
+                關於
+            </a>
+            <a class="item" href="javascript:void(0)">
+                <i class="spy icon"></i>
+                登入/註冊
+            </a>
         </div>
 
         {{-- Content --}}
@@ -33,6 +49,8 @@
         {{-- Javascript --}}
         {!! Html::script('//code.jquery.com/jquery-3.0.0.min.js') !!}
         {!! Html::script('semantic/semantic.js') !!}
+        {!! Html::script('semantic/components/sidebar.js') !!}
+        {!! Html::script('semantic/components/visibility.js') !!}
         @yield('js')
     </body>
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,146 @@
 @extends('app')
 
+@section('css')
+    <style>
+
+        .masthead.segment {
+            min-height: 700px;
+            padding: 1em 0em;
+        }
+        .masthead .logo.item img {
+            margin-right: 1em;
+        }
+        .masthead .ui.menu .ui.button {
+            margin-left: 0.5em;
+        }
+        .masthead h1.ui.header {
+            margin-top: 3em;
+            margin-bottom: 0em;
+            font-size: 4em;
+            font-weight: normal;
+        }
+        .masthead h2 {
+            font-size: 1.7em;
+            font-weight: normal;
+        }
+        .secondary.pointing.menu {
+            border: none !important;
+        }
+
+        .secondary.pointing.menu .toc.item {
+            display: none;
+
+        }
+
+        @media only screen and (max-width: 700px) {
+            .ui.fixed.menu {
+                display: none !important;
+            }
+            .secondary.pointing.menu .item,
+            .secondary.pointing.menu .menu {
+                display: none;
+            }
+            .secondary.pointing.menu .toc.item {
+                display: block;
+            }
+            .masthead.segment {
+                min-height: 350px;
+            }
+            .masthead h1.ui.header {
+                font-size: 2em;
+                margin-top: 1.5em;
+            }
+            .masthead h2 {
+                margin-top: 0.5em;
+                font-size: 1.5em;
+            }
+        }
+        #header {
+            min-height: 100vh !important;
+            background-color: rgba(0, 0, 0, 0);
+        }
+        #bg {
+            position: fixed;
+            z-index: 1;
+            width: 100%;
+            min-height: 100vh !important;
+        }
+        #footer {
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+            margin-bottom: 10px;
+        }
+        .background img, .background video{
+            min-height:100vh;
+            min-width:100%;
+        }
+        .frame.curtain{
+            position: relative;
+            z-index: 2;
+        }
+        .white {
+            color: #ffffff;
+        }
+    </style>
+@endsection
+
 @section('title', '首頁')
 
 @section('content')
-    <div class="pusher">
-
+    <div class="background frame" id="bg">
+        <video autoplay loop muted>
+            <source src="//i.imgur.com/2r5Ldv5.mp4" type="video/mp4">
+        </video>
     </div>
+    <div class="pusher frame curtain" id="header">
+        <div class="ui vertical masthead center aligned segment">
+            {{--<div class="ui container">--}}
+                {{--<div class="ui large secondary inverted pointing menu">--}}
+                    {{--<a class="toc item">--}}
+                        {{--<i class="sidebar icon"></i>--}}
+                    {{--</a>--}}
+                    {{--<a class="active item" href="javascript:void(0)">首頁</a>--}}
+                    {{--<a class="item" href="javascript:void(0)">題目</a>--}}
+                    {{--<a class="item" href="javascript:void(0)">關於</a>--}}
+                    {{--<div class="right item">--}}
+                        {{--<a class="ui inverted button" href="javascript:void(0)">登入/註冊</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+
+            <div class="ui text container">
+                <h1 class="ui inverted header">
+                    HackerSir CTF
+                </h1>
+                <h2 class="ui white">Wanna try your hacker skill? Just try it.</h2>
+                <div class="ui huge primary button">Try It!<i class="right arrow icon"></i></div>
+            </div>
+
+            <div class="ui inverted vertical footer" id="footer">
+                <div class="ui container center aligned">
+                    <p class="white">Copyright (c) 2016 HackerSir, All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            // fix menu when passed
+            $('.masthead').visibility({
+                once: false,
+                onBottomPassed: function() {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
+            // create sidebar and attach to menu open
+            $('.ui.labeled.icon.sidebar').sidebar('attach events', '.toc.item');
+        });
+    </script>
 @endsection
