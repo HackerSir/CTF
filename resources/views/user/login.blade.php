d5f92b0c5363218c7282ecc9a5828a9ad91ff8bc
KID <jyhsu2000@yahoo.com.tw>
2016-07-05 18:06:07 +0800

    Add container with an segment inside

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -19,40 +19,46 @@
 
 @section('content')
     {{-- FIXME: 垂直位置 --}}
-    <div class="ui large aligned center aligned relaxed stackable grid" style="margin-top: calc(50vh - 130px)">
-        <div class="six wide column">
-            <h2 class="ui teal image header">
-                Login
-            </h2>
-            <form class="ui large form">
-                <div class="ui stacked segment">
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="user icon"></i>
-                            <input type="text" name="email" placeholder="E-mail address">
+    <div class="ui container">
+        <div class="ui top attached segment">
+            <div class="ui top attached label">登入</div>
+            <div class="ui large aligned center aligned relaxed stackable grid">
+                <div class="six wide column">
+                    <h2 class="ui teal image header">
+                        Login
+                    </h2>
+                    <form class="ui large form">
+                        <div class="ui stacked segment">
+                            <div class="field">
+                                <div class="ui left icon input">
+                                    <i class="user icon"></i>
+                                    <input type="text" name="email" placeholder="E-mail address">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="ui left icon input">
+                                    <i class="lock icon"></i>
+                                    <input type="password" name="password" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="ui fluid large teal submit button">Login</div>
                         </div>
-                    </div>
-                    <div class="field">
-                        <div class="ui left icon input">
-                            <i class="lock icon"></i>
-                            <input type="password" name="password" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="ui fluid large teal submit button">Login</div>
-                </div>
 
-                <div class="ui error message"></div>
-            </form>
-        </div>
-        <div class="ui vertical divider" style="height: 25% !important;">OR</div>
-        <div class="six wide column">
-            <h2>No Account?</h2>
-            <p>You can just Sign up!</p>
-            <div class="ui horizontal divider">
-                Or
+                        <div class="ui error message"></div>
+                    </form>
+                </div>
+                <div class="ui vertical divider" style="height: 25% !important;">OR</div>
+                <div class="six wide column">
+                    <h2>No Account?</h2>
+                    <p>You can just Sign up!</p>
+                    <div class="ui horizontal divider">
+                        Or
+                    </div>
+                    <h2>Forgot Password?</h2>
+                    <p>Well, now you can change your password.</p>
+                </div>
             </div>
-            <h2>Forgot Password?</h2>
-            <p>Well, now you can change your password.</p>
         </div>
     </div>
+
 @endsection
