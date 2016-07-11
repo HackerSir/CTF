e790b292e44aa63a5019e758f007ea28a0f3b7e5
KID <jyhsu2000@yahoo.com.tw>
2016-07-11 16:36:21 +0800

    Update user show page

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,82 +3,78 @@
 @section('title', "{$user->name} - 會員資料")
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{ $user->name }} - 會員資料</div>
-                    {{-- Panel body --}}
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="text-center">
-                                {{-- Gravatar大頭貼 --}}
-                                <img src="{{ Gravatar::src($user->email, 200) }}" class="img-circle" /><br />
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="row">
-                            <div class="text-center col-md-10 col-md-offset-1">
-                                <table class="table table-hover">
-                                    <tr>
-                                        <td class="text-right col-md-4">名稱：</td>
-                                        <td>{{ $user->name }}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-right">Email：</td>
-                                        <td>
-                                            {{ $user->email }}
-                                            @if (!$user->isConfirmed)
-                                                <i class="fa fa-exclamation-triangle text-danger" title="尚未完成信箱驗證"></i>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-right">角色：</td>
-                                        <td>
-                                            @foreach($user->roles as $role)
-                                                {{ $role->display_name }}<br />
-                                            @endforeach
-                                        </td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
-                        <hr />
-                        <div class="row">
-                            <div class="text-center col-md-10 col-md-offset-1">
-                                <table class="table table-hover">
-                                    <tr>
-                                        <td class="text-right col-md-4">註冊時間：</td>
-                                        <td>{{ $user->register_at }}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-right">註冊IP：</td>
-                                        <td>{{ $user->register_ip }}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-right">最後登入時間：</td>
-                                        <td>{{ $user->last_login_at }}</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="text-right">最後登入IP：</td>
-                                        <td>{{ $user->last_login_ip }}</td>
-                                    </tr>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="text-center">
-                    <a href="{{ route('user.index') }}" class="btn btn-default"><i class="fa fa-arrow-left"></i> 會員清單</a>
-                    <a href="{{ route('user.edit', $user) }}" class="btn btn-primary" title=""><i class="fa fa-pencil"></i> 編輯會員</a>
-                    {!! Form::open(['route' => ['user.destroy', $user], 'style' => 'display: inline', 'method' => 'DELETE', 'onSubmit' => "return confirm('確定要刪除此會員嗎？');"]) !!}
-                    <button type="submit" class="btn btn-danger">
-                        <i class="fa fa-trash"></i> 刪除會員
-                    </button>
-                    {!! Form::close() !!}
-                </div>
+    <div class="ui container center aligned">
+        <h2 class="ui teal image header">
+            {{ $user->name }} - 會員資料
+        </h2>
+        {{-- TODO: 麵包屑抽出來（建議）--}}
+        {{-- FIXME: 靠左對齊--}}
+        <div class="ui grey message">
+            <div class="ui breadcrumb">
+                <div class="section">現在位置：</div>
+                <a class="section">{{ link_to_route('user.index', '會員清單') }}</a>
+                <i class="right arrow icon divider"></i>
+                <div class="active section">會員資料</div>
+                <i class="right arrow icon divider"></i>
+                <div class="active section">{{ $user->name }}</div>
             </div>
+        </div>
+        <div>
+            {{-- Gravatar大頭貼 --}}
+            {{-- FIXME: 連結太寬 --}}
+            <a href="https://zh-tw.gravatar.com/" target="_blank" title="透過Gravatar更換照片">
+                <img src="{{ Gravatar::src($user->email, 200) }}" class="ui centered medium circular image"
+                     id="gravatar"/></a><br/>
+        </div>
+
+        <table class="ui selectable stackable table">
+            <tr>
+                <td class="four wide right aligned">名稱：</td>
+                <td>{{ $user->name }}</td>
+            </tr>
+            <tr>
+                <td class="right aligned">Email：</td>
+                <td>
+                    {{ $user->email }}
+                    @if (!$user->isConfirmed)
+                        <i class="warning sign icon red" title="尚未完成信箱驗證"></i>
+                    @endif
+                </td>
+            </tr>
+            <tr>
+                <td class="right aligned">角色：</td>
+                <td>
+                    @foreach($user->roles as $role)
+                        {{ $role->display_name }}<br/>
+                    @endforeach
+                </td>
+            </tr>
+            <tr>
+                <td class="four wide right aligned">註冊時間：</td>
+                <td>{{ $user->register_at }}</td>
+            </tr>
+            <tr>
+                <td class="right aligned">註冊IP：</td>
+                <td>{{ $user->register_ip }}</td>
+            </tr>
+            <tr>
+                <td class="right aligned">最後登入時間：</td>
+                <td>{{ $user->last_login_at }}</td>
+            </tr>
+            <tr>
+                <td class="right aligned">最後登入IP：</td>
+                <td>{{ $user->last_login_ip }}</td>
+            </tr>
+        </table>
+
+        <div>
+            <a href="{{ route('user.index') }}" class="ui icon button"><i class="arrow left icon"></i> 會員清單</a>
+            <a href="{{ route('user.edit', $user) }}" class="ui icon button"><i class="edit icon"></i> 編輯資料</a>
+            {!! Form::open(['route' => ['user.destroy', $user], 'style' => 'display: inline', 'method' => 'DELETE', 'onSubmit' => "return confirm('確定要刪除此會員嗎？');"]) !!}
+            <button type="submit" class="ui icon button red">
+                <i class="trash icon"></i> 刪除會員
+            </button>
+            {!! Form::close() !!}
         </div>
     </div>
 @endsection
