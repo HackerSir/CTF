18db86398e26d11ec8930995e210f0c053150fc2
KID <jyhsu2000@yahoo.com.tw>
2016-07-11 18:59:37 +0800

    Bug found

--- resources/views/navbar/item.blade.php
+++ resources/views/navbar/item.blade.php
@@ -14,7 +14,11 @@
         {{-- 一般項目 --}}
         @if($item->link)
             {{-- 超連結 --}}
-            <a @lm-attrs($item) class="item" @lm-endattrs href="{!! $item->url() !!}">{!! $item->title !!}</a>
+            {{-- FIXME: @lm-attrs 無法同時處理兩種情況
+            自動判斷active：@lm-attrs($item->link)
+            令LaravelMenu可直接調整或追加屬性：@lm-attrs($item)
+            --}}
+            <a @lm-attrs($item->link) class="item" @lm-endattrs href="{!! $item->url() !!}">{!! $item->title !!}</a>
         @else
             {{-- 文字 --}}
             {!! $item->title !!}
