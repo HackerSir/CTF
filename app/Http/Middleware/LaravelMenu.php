aa50fbc080a475b60cba2df216a47706cf6da859
KID <jyhsu2000@yahoo.com.tw>
2016-07-11 14:50:45 +0800

    Update route and navbar

--- app/Http/Middleware/LaravelMenu.php
+++ app/Http/Middleware/LaravelMenu.php
@@ -47,66 +47,69 @@ class LaravelMenu
                     //FIXME: 無法追記加class
                     $confirmAlert->attr('class', 'red');
                 }
+                if (Entrust::can(['user.manage', 'user.view'])) {
+                    $menu->add('會員清單', ['route' => 'user.index']);
+                }
                 $menu->add('個人資料', ['route' => 'profile'])->active('profile/*');
                 $menu->add('登出', ['action' => 'Auth\AuthController@logout']);
-            }else{
-                //遊客
-                $menu->add('登入', ['action' => 'Auth\AuthController@showLoginForm']);
-            }
-        });
-        //FIXME
-        Menu::make('navBar', function ($menu) {
-            /** @var \Lavary\Menu\Builder $menu */
-            //基本巡覽列
-
-            //會員
-            if (Auth::check()) {
-                //信箱驗證
-                if (!Auth::user()->isConfirmed) {
-                    $confirmAlert = $menu->add(
-                        '<i class="fa fa-exclamation-triangle"></i> 尚未完成信箱驗證',
-                        ['route' => 'auth.resend-confirm-mail']
-                    );
-                    $confirmAlert->attr('class', 'alert-danger');
-                }
-                //核心功能
-                if (Entrust::can(['unit.view', 'unit.manage'])) {
-                    $menu->add('單位', ['route' => 'unit.index'])->active('unit/*');
-                }
-                if (Entrust::can(['iodef.view', 'iodef.manage'])) {
-                    $menu->add('IODEF', ['route' => 'IODEF.index'])->active('IODEF/*');
-                }
-                if (Entrust::can(['association.view', 'association.manage'])) {
-                    $menu->add('Association', ['route' => 'association.index'])->active('association/*');
-                }
-
-                if (Entrust::hasRole('admin')) {
-                    /** @var \Lavary\Menu\Builder $adminMenu */
-                    $adminMenu = $menu->add('管理員', 'javascript:void(0)');
-                    $adminMenu->add('會員管理', ['route' => 'user.index']);
-                    $adminMenu->add('權限清單', ['route' => 'permission.index']);
-                    $adminMenu->add('網站設定', ['route' => 'setting.index'])->divide();
-                    $adminMenu->add('測試面板', ['route' => 'test.index'])->active('test/*')->divide();
-                    $adminMenu->add(
-                        '記錄檢視器 <i class="fa fa-external-link"></i>',
-                        ['route' => 'log-viewer::dashboard']
-                    )->link->attr('target', '_blank');
-                }
-                //會員
-                /** @var \Lavary\Menu\Builder $userMenu */
-                $userMenu = $menu->add(
-                    '<img src="' . Gravatar::src(Auth::user()->email) . '" class="img-circle" height=40 width=40> '
-                    . Auth::user()->name,
-                    'javascript:void(0)'
-                );
-                $userMenu->link->attr('style', 'padding: 5px 15px 5px 5px');
-                $userMenu->add('個人資料', ['route' => 'profile'])->active('profile/*');
-                $userMenu->add('登出', ['action' => 'Auth\AuthController@logout']);
             } else {
                 //遊客
                 $menu->add('登入', ['action' => 'Auth\AuthController@showLoginForm']);
             }
         });
+        //FIXME
+//        Menu::make('navBar', function ($menu) {
+//            /** @var \Lavary\Menu\Builder $menu */
+//            //基本巡覽列
+//
+//            //會員
+//            if (Auth::check()) {
+//                //信箱驗證
+//                if (!Auth::user()->isConfirmed) {
+//                    $confirmAlert = $menu->add(
+//                        '<i class="fa fa-exclamation-triangle"></i> 尚未完成信箱驗證',
+//                        ['route' => 'auth.resend-confirm-mail']
+//                    );
+//                    $confirmAlert->attr('class', 'alert-danger');
+//                }
+//                //核心功能
+//                if (Entrust::can(['unit.view', 'unit.manage'])) {
+//                    $menu->add('單位', ['route' => 'unit.index'])->active('unit/*');
+//                }
+//                if (Entrust::can(['iodef.view', 'iodef.manage'])) {
+//                    $menu->add('IODEF', ['route' => 'IODEF.index'])->active('IODEF/*');
+//                }
+//                if (Entrust::can(['association.view', 'association.manage'])) {
+//                    $menu->add('Association', ['route' => 'association.index'])->active('association/*');
+//                }
+//
+//                if (Entrust::hasRole('admin')) {
+//                    /** @var \Lavary\Menu\Builder $adminMenu */
+//                    $adminMenu = $menu->add('管理員', 'javascript:void(0)');
+//                    $adminMenu->add('會員管理', ['route' => 'user.index']);
+//                    $adminMenu->add('權限清單', ['route' => 'permission.index']);
+//                    $adminMenu->add('網站設定', ['route' => 'setting.index'])->divide();
+//                    $adminMenu->add('測試面板', ['route' => 'test.index'])->active('test/*')->divide();
+//                    $adminMenu->add(
+//                        '記錄檢視器 <i class="fa fa-external-link"></i>',
+//                        ['route' => 'log-viewer::dashboard']
+//                    )->link->attr('target', '_blank');
+//                }
+//                //會員
+//                /** @var \Lavary\Menu\Builder $userMenu */
+//                $userMenu = $menu->add(
+//                    '<img src="' . Gravatar::src(Auth::user()->email) . '" class="img-circle" height=40 width=40> '
+//                    . Auth::user()->name,
+//                    'javascript:void(0)'
+//                );
+//                $userMenu->link->attr('style', 'padding: 5px 15px 5px 5px');
+//                $userMenu->add('個人資料', ['route' => 'profile'])->active('profile/*');
+//                $userMenu->add('登出', ['action' => 'Auth\AuthController@logout']);
+//            } else {
+//                //遊客
+//                $menu->add('登入', ['action' => 'Auth\AuthController@showLoginForm']);
+//            }
+//        });
         return $next($request);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -23,6 +23,14 @@ Route::get('/', ['as' => 'index', function () {
 
 //會員（須完成信箱驗證）
 Route::group(['middleware' => 'email'], function () {
+    //會員管理
+    //權限：user.manage、user.view
+    Route::resource('user', 'UserController', [
+        'except' => [
+            'create',
+            'store'
+        ]
+    ]);
     //會員資料
     Route::group(['prefix' => 'profile'], function () {
         //查看會員資料
