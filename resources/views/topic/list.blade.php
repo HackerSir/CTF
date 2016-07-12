01df296583bb49bcc099cf619b9f2fa3ebbc7a4b
Vongola <vongola12324@coder.tw>
2016-07-12 23:19:59 +0800

    Add topic list and show

--- resources/views/topic/list.blade.php
+++ resources/views/topic/list.blade.php
@@ -1,1 +1,54 @@
 @extends('app')
+
+@section('title', '題目清單')
+
+@section('css')
+    <style>
+        table {
+            border: none !important;
+        }
+    </style>
+@endsection
+
+@section('content')
+    <div class="ui container">
+        <h2 class="ui teal header center aligned">
+            題目清單
+        </h2>
+        <table class="ui very basic selectable celled table">
+            <thead>
+                <tr>
+                    <th class="one wide">序號</th>
+                    <th class="one wide">分類</th>
+                    <th class="one wide">題號</th>
+                    <th class="six wide">題目名稱</th>
+                    <th class="one wide">通過率</th>
+                    <th class="three wide">加入時間</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Misc</td>
+                    <td>a001</td>
+                    <td>Hello World</td>
+                    <td>100%</td>
+                    <td>2038/01/19</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Web</td>
+                    <td>b001</td>
+                    <td>Hello Internet</td>
+                    <td>99%</td>
+                    <td>2038/01/19</td>
+                </tr>
+            </tbody>
+        </table>
+        <div style="text-align: center;">
+            {{--{!! $users->appends(Request::except(['page']))->render() !!}--}}
+            <a href="{{ route('topic.index') }}" class="ui blue inverted icon button"><i class="arrow left icon"></i>
+                題目分類</a>
+        </div>
+    </div>
+@endsection
--- resources/views/topic/show.blade.php
+++ resources/views/topic/show.blade.php
@@ -1,1 +1,43 @@
 @extends('app')
+
+
+@section('title', 'a001 - Hello World')
+
+@section('content')
+    <div class="ui container">
+        <h2 class="ui teal header center aligned">
+            a001 - Hello World
+        </h2>
+        <div class="ui divided grid">
+            <div class="row">
+                <div class="ten wide column">
+                    <h3>分類：MISC</h3>
+
+                </div>
+                <div class="ui fitted vertical divider"></div>
+                <div class="two wide column">
+                    <h3>本題提示：</h3>
+                    <ul>
+                        <li>沒有提示。</li>
+                        <li>沒提示。</li>
+                        <li>沒提。</li>
+                        <li>沒。</li>
+                        <li>。</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="ui horizontal divider">And</div>
+        <div class="ui text container" style="text-align: center">
+            <h3>Flag</h3>
+            <form action="post" class="ui form large">
+                <div class="field required">
+                    <input type="text" name="name" placeholder="Name / Nickname" required="required">
+                </div>
+                <button type="submit" class="ui blue inverted submit button">送出</button>
+                <a href="javascript:void(0)" class="ui blue inverted icon button " title="編輯題目"><i
+                            class="edit icon"></i>編輯題目</a>
+            </form>
+        </div>
+    </div>
+@endsection
