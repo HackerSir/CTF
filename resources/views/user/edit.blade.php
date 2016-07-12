19de677e801e474cc7083885d6242d410041e6cf
Vongola <vongola12324@coder.tw>
2016-07-12 15:54:57 +0800

    Fix user.edit

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -17,56 +17,53 @@
                 <div class="active section">編輯會員資料</div>
             </div>
         </div>
-        <div class="ui top attached segment">
-            <div class="ui top attached label">{{ $user->name }} - 編輯會員資料</div>
-            <div class="ui large aligned center aligned relaxed stackable grid">
-                <div class="ten wide column">
-                    <h2 class="ui teal image header">
-                        {{ $user->name }} - 編輯會員資料
-                    </h2>
-                    {!! SemanticForm::open()->put()->action(route('user.update', $user))->addClass('large') !!}
-                    {!! SemanticForm::bind($user) !!}
-                    <div class="ui stacked segment">
-                        {!! SemanticForm::text('email')->label('Email')->placeholder('Email')->disable() !!}
-                        <div class="ui tiny negative message">
-                            <ul class="list">
-                                <li>信箱作為帳號使用，故無法修改</li>
-                            </ul>
-                        </div>
-                        {!! SemanticForm::text('name')->label('Name')->placeholder('Name / Nickname')->required() !!}
-                        <div class="field{{ ($errors->has('role'))?' error':'' }}">
-                            <label>角色</label>
-                            @foreach($roles as $role)
-                                <div class="ui checkbox">
-                                    @if($user->id == Auth::user()->id && $role->name == 'admin')
-                                        {!! Form::checkbox('role[]', $role->id, $user->hasRole($role->name), ['disabled']) !!}
-                                        <label>{{ $role->display_name }} </label>
-                                        <span class="ui small label red">禁止解除自己的管理員職務</span>
-                                    @else
-                                        {!! Form::checkbox('role[]', $role->id, $user->hasRole($role->name)) !!}
-                                        <label>{{ $role->display_name }} </label>
-                                    @endif
-                                </div>
-                                <br/>
-                            @endforeach
-                        </div>
-                        <a href="{{ route('user.show', $user) }}" class="ui button"><i class="icon arrow left"></i>
-                            返回會員資料</a>
-                        {!! SemanticForm::submit('Update profile')->addClass('teal submit') !!}
-                    </div>
 
-                    @if($errors->count())
-                        <div class="ui error message" style="display: block">
-                            <ul class="list">
-                                @foreach($errors->all('<li>:message</li>') as $error)
-                                    {!! $error !!}
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    {!! SemanticForm::close() !!}
-                </div>
+        <h2 class="ui teal header center aligned">
+            {{ $user->name }} - 編輯會員資料
+        </h2>
+        {!! SemanticForm::open()->put()->action(route('user.update', $user))->addClass('large') !!}
+        {!! SemanticForm::bind($user) !!}
+        <div class="ui stacked segment">
+            {!! SemanticForm::text('email')->label('Email')->placeholder('Email')->disable() !!}
+            <div class="ui tiny negative message">
+                <ul class="list">
+                    <li>信箱作為帳號使用，故無法修改</li>
+                </ul>
+            </div>
+            {!! SemanticForm::text('name')->label('Name')->placeholder('Name / Nickname')->required() !!}
+            <div class="field{{ ($errors->has('role'))?' error':'' }}">
+                <label>角色</label>
+                @foreach($roles as $role)
+                    <div class="ui checkbox">
+                        @if($user->id == Auth::user()->id && $role->name == 'admin')
+                            {!! Form::checkbox('role[]', $role->id, $user->hasRole($role->name), ['disabled']) !!}
+                            {{--  TODO: 這邊我改用<i>去放，覺得不好再改回來吧 --}}
+                            <label>{{ $role->display_name }} <i class="warning sign icon red" title="禁止解除自己的管理員職務"></i></label>
+                        @else
+                            {!! Form::checkbox('role[]', $role->id, $user->hasRole($role->name)) !!}
+                            <label>{{ $role->display_name }} </label>
+                        @endif
+                    </div>
+                    <br/>
+                @endforeach
+            </div>
+            <div style="text-align: center">
+                <a href="{{ route('user.show', $user) }}" class="ui blue basic icon button"><i class="icon arrow left"></i>
+                    返回會員資料</a>
+                {!! SemanticForm::submit('Update profile')->addClass('ui red basic submit button') !!}
             </div>
         </div>
+
+        @if($errors->count())
+            <div class="ui error message" style="display: block">
+                <ul class="list">
+                    @foreach($errors->all('<li>:message</li>') as $error)
+                        {!! $error !!}
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        {!! SemanticForm::close() !!}
     </div>
+
 @endsection
