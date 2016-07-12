533a51e0314e7441ce26102c9dd9cf0d497ae815
Vongola <vongola12324@coder.tw>
2016-07-12 15:39:24 +0800

    Fix profile.edit

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,39 +4,34 @@
 
 @section('content')
     <div class="ui container">
-        <div class="ui top attached segment">
-            <div class="ui top attached label">編輯個人資料</div>
-            <div class="ui large aligned center aligned relaxed stackable grid">
-                <div class="ten wide column">
-                    <h2 class="ui teal image header">
-                        Edit profile
-                    </h2>
-                    {!! SemanticForm::open()->put()->action(route('profile.update'))->addClass('large') !!}
-                    {!! SemanticForm::bind($user) !!}
-                    <div class="ui stacked segment">
-                        {!! SemanticForm::text('email')->label('Email')->placeholder('Email')->disable() !!}
-                        <div class="ui tiny negative message">
-                            <ul class="list">
-                                <li>信箱作為帳號使用，故無法修改</li>
-                            </ul>
-                        </div>
-                        {!! SemanticForm::text('name')->label('Name')->placeholder('Name / Nickname')->required() !!}
-                        <a href="{{ route('profile') }}" class="ui button"><i class="icon arrow left"></i> 返回個人資料</a>
-                        {!! SemanticForm::submit('Update profile')->addClass('teal submit') !!}
-                    </div>
+        <h2 class="ui teal header center aligned">編輯個人資料</h2>
+        {!! SemanticForm::open()->put()->action(route('profile.update'))->addClass('large') !!}
+        {!! SemanticForm::bind($user) !!}
+        <div class="ui stacked segment">
+            {!! SemanticForm::text('email')->label('Email')->placeholder('Email')->disable() !!}
+            <div class="ui tiny negative message">
+                <ul class="list">
+                    <li>信箱作為帳號使用，故無法修改</li>
+                </ul>
+            </div>
+            {!! SemanticForm::text('name')->label('Name')->placeholder('Name / Nickname')->required() !!}
 
-                    @if($errors->count())
-                        <div class="ui error message" style="display: block">
-                            <ul class="list">
-                                @foreach($errors->all('<li>:message</li>') as $error)
-                                    {!! $error !!}
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    {!! SemanticForm::close() !!}
-                </div>
+            {{-- TODO: text-align: center要獨立成一個text-center --}}
+            <div style="text-align: center">
+                <a href="{{ route('profile') }}" class="ui red basic icon button"><i class="icon arrow left"></i> 返回個人資料</a>
+                {!! SemanticForm::submit('Update profile')->addClass('ui teal basic submit button') !!}
             </div>
         </div>
+
+        @if($errors->count())
+            <div class="ui error message" style="display: block">
+                <ul class="list">
+                    @foreach($errors->all('<li>:message</li>') as $error)
+                        {!! $error !!}
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        {!! SemanticForm::close() !!}
     </div>
 @endsection
