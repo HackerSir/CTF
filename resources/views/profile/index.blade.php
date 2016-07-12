eaa503a539fe98b98db17c2950bbf6a8b1400e64
Vongola <vongola12324@coder.tw>
2016-07-12 15:30:48 +0800

    Fix profile.index

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -6,69 +6,71 @@
     <style>
         #gravatar {
             border: 3px solid white;
+            margin-left: auto !important;
+            margin-right: auto !important;
         }
         #gravatar:hover {
             border: 3px dotted black;
+
+        /* TODO: 看要置左還是置中，然後看要不要抽出來做*/
+        @media only screen and (max-width: 767px) {
+            td {
+                text-align: center !important;
+            }
         }
     </style>
 @endsection
 
 @section('content')
     <div class="ui container">
-        <div class="ui top attached segment">
-            <div class="ui top attached label">個人資料</div>
-            <div class="ui large aligned center aligned relaxed stackable grid">
-                <div class="eight wide column">
-                    <h2 class="ui teal image header">
-                        Profile
-                    </h2>
-                    <div>
-                        {{-- Gravatar大頭貼 --}}
-                        <a href="https://zh-tw.gravatar.com/" target="_blank" title="透過Gravatar更換照片">
-                            <img src="{{ Gravatar::src($user->email, 200) }}" class="ui centered medium circular image" id="gravatar" /></a><br />
-                    </div>
-
-                    <table class="ui selectable stackable table">
-                        <tr>
-                            <td class="four wide right aligned">名稱：</td>
-                            <td>{{ $user->name }}</td>
-                        </tr>
-                        <tr>
-                            <td class="right aligned">Email：</td>
-                            <td>{{ $user->email }}</td>
-                        </tr>
-                        <tr>
-                            <td class="right aligned">角色：</td>
-                            <td>
-                                @foreach($user->roles as $role)
-                                    {{ $role->display_name }}<br />
-                                @endforeach
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="four wide right aligned">註冊時間：</td>
-                            <td>{{ $user->register_at }}</td>
-                        </tr>
-                        <tr>
-                            <td class="right aligned">註冊IP：</td>
-                            <td>{{ $user->register_ip }}</td>
-                        </tr>
-                        <tr>
-                            <td class="right aligned">最後登入時間：</td>
-                            <td>{{ $user->last_login_at }}</td>
-                        </tr>
-                        <tr>
-                            <td class="right aligned">最後登入IP：</td>
-                            <td>{{ $user->last_login_ip }}</td>
-                        </tr>
-                    </table>
-
-                    <div>
-                        <a href="{{ route('profile.edit') }}" class="ui button"><i class="icon edit"></i> 編輯資料</a>
-                        <a href="{{ route('profile.change-password') }}" class="ui button"><i class="icon lock"></i> 修改密碼</a>
-                    </div>
-                </div>
+        {{--<div class="ui aligned center aligned">--}}
+            <h2 class="ui teal header center aligned">個人資料</h2>
+            <div class="ui header center aligned">
+                {{-- Gravatar大頭貼 --}}
+                <a href="https://zh-tw.gravatar.com/" target="_blank" title="透過Gravatar更換照片" class="ui medium circular image">
+                    <img src="{{ Gravatar::src($user->email, 200) }}" class="ui image" id="gravatar" /></a><br />
             </div>
-        </div>
+
+            <table class="ui selectable stackable table" id="userInfo">
+                <tr>
+                    <td class="four wide right aligned">名稱：</td>
+                    <td>{{ $user->name }}</td>
+                </tr>
+                <tr>
+                    <td class="right aligned">Email：</td>
+                    <td>{{ $user->email }}</td>
+                </tr>
+                <tr>
+                    <td class="right aligned">角色：</td>
+                    <td>
+                        @foreach($user->roles as $role)
+                            {{ $role->display_name }}<br />
+                        @endforeach
+                    </td>
+                </tr>
+                <tr>
+                    <td class="four wide right aligned">註冊時間：</td>
+                    <td>{{ $user->register_at }}</td>
+                </tr>
+                <tr>
+                    <td class="right aligned">註冊IP：</td>
+                    <td>{{ $user->register_ip }}</td>
+                </tr>
+                <tr>
+                    <td class="right aligned">最後登入時間：</td>
+                    <td>{{ $user->last_login_at }}</td>
+                </tr>
+                <tr>
+                    <td class="right aligned">最後登入IP：</td>
+                    <td>{{ $user->last_login_ip }}</td>
+                </tr>
+            </table>
+
+            {{-- TODO: text-align: center要獨立成一個text-center --}}
+            <div style="text-align: center">
+                <a href="{{ route('profile.edit') }}" class="ui icon red basic button"><i class="icon edit"></i> 編輯資料</a>
+                <a href="{{ route('profile.change-password') }}" class="ui icon brown basic button"><i class="icon lock"></i> 修改密碼</a>
+            </div>
+        {{--</div>--}}
     </div>
 @endsection
