93de1ac6d8908eebbe2f465b5db8ff7c774977e1
KID <jyhsu2000@yahoo.com.tw>
2016-07-28 14:45:28 +0800

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -88,24 +88,23 @@ Route::group(['middleware' => 'auth'], function () {
 //會員系統
 //麵包屑要求對全部路由命名，因此將 Route::auth() 複製出來自己命名
 //Route::auth();
-// Authentication Routes...
-Route::get('login', 'Auth\AuthController@showLoginForm')->name('auth.login');
-Route::post('login', 'Auth\AuthController@login')->name('auth.login');
-Route::get('logout', 'Auth\AuthController@logout')->name('auth.logout');
-// Registration Routes...
-Route::get('register', 'Auth\AuthController@showRegistrationForm')->name('auth.register');
-Route::post('register', 'Auth\AuthController@register')->name('auth.register');
-// Password Reset Routes...
-Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm')->name('auth.password.reset');
-Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail')->name('auth.password.email');
-Route::post('password/reset', 'Auth\PasswordController@reset')->name('auth.password.reset');
+Route::group(['namespace' => 'Auth'], function () {
+    // Authentication Routes...
+    Route::get('login', 'AuthController@showLoginForm')->name('auth.login');
+    Route::post('login', 'AuthController@login')->name('auth.login');
+    Route::get('logout', 'AuthController@logout')->name('auth.logout');
+    // Registration Routes...
+    Route::get('register', 'AuthController@showRegistrationForm')->name('auth.register');
+    Route::post('register', 'AuthController@register')->name('auth.register');
+    // Password Reset Routes...
+    Route::get('password/reset/{token?}', 'PasswordController@showResetForm')->name('auth.password.reset');
+    Route::post('password/email', 'PasswordController@sendResetLinkEmail')->name('auth.password.email');
+    Route::post('password/reset', 'PasswordController@reset')->name('auth.password.reset');
 
-//修改密碼
-Route::get('change-password', 'Auth\PasswordController@getChangePassword')->name('auth.change-password');
-Route::put('update-password', 'Auth\PasswordController@updatePassword')->name('auth.update-password');
+    //修改密碼
+    Route::get('change-password', 'PasswordController@getChangePassword')->name('auth.change-password');
+    Route::put('update-password', 'PasswordController@updatePassword')->name('auth.update-password');
 
-//驗證信箱
-Route::get('confirm/{confirmCode}', [
-    'as'   => 'auth.confirm',
-    'uses' => 'Auth\AuthController@emailConfirm'
-]);
+    //驗證信箱
+    Route::get('confirm/{confirmCode}', 'AuthController@emailConfirm')->name('auth.confirm');
+});
