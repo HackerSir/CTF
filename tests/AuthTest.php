1414c05fe05e23ff98b3673946b403c9dd68718e
Chan, Danny <danny5010@yahoo.com.tw>
2016-08-04 17:42:32 +0800

    Add AuthTest.testRegister

---
+++ tests/AuthTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Hackersir\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AuthTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // 準備一組帳號做登入測試
+
+    }
+
+    public function testRegister() {
+        // arrange
+        $userName = 'CTF';
+        $userEmail = 'ctf@example.com';
+        $userPassword = 'ctf1234';
+
+        // act
+        $this->visit('register')
+            ->type($userName, 'name')
+            ->type($userEmail, 'email')
+            ->type($userPassword, 'password')
+            ->type($userPassword, 'password_confirmation')
+            ->press('Register')
+            ->seePageIs('/');
+
+        // assert
+        $user = User::where('email', $userEmail)->first();
+        $this->assertEquals($userName, $user->name);
+        $this->assertTrue(Hash::check($userPassword, $user->getAuthPassword()));
+    }
+
+    public function testLogin() {
+
+    }
+
+}
